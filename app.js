fe66f2b6eaa413698ae59885860c33cb71ad6079
Waallin <viktorwallin@hotmail.se>
2022-01-25 16:45:30 +0100

    update

--- app.js
+++ app.js
@@ -1,70 +1,152 @@
 
 
 $(document).ready(function () {
+
+
+
     let numbersAdded = []; //array att samla tal
 
+    function add() { //plusfunktion
 
-    $('#add').click(function () { //Plusknappen
-
-        let calcText = '';
-
+        if ($('#sumP').html.length > 1) {
+            $('#sumP').html('')
+        }
 
         //lägger till array som gör uträkning
-        let addNumber = parseInt($('#text').val())
-        numbersAdded.push(addNumber)
 
-        if (numbersAdded.length > 1) {
-            calcText = $('#text').val();
-            $('#calc').append(' + ' + calcText);
-            calcText = '';
+        let addNumber = parseFloat($('#text').val())
 
-        } else {
+        if (numbersAdded == 0) { //tar bort uträkning för en ny uträkning
+            $('#sumP').html('');
+        }
 
-            //lägger till i HTML
-            calcText = $('#text').val();
-            $('#calc').append(calcText);
-            calcText = '';
+        if (!isNaN(addNumber)) { //Checkar så att det är ett nummer innan det läggs till i arrayen
+            numbersAdded.push(addNumber)
 
+            if (numbersAdded.length > 1) {
+
+                calcText = $('#text').val();
+                $('#calc').append(' + ' + calcText);
+                $('#text').val('')
+
+            } else {
+
+                //lägger till i HTML
+                calcText = $('#text').val();
+                $('#calc').append(calcText);
+                $('#text').val('')
+            }
         }
 
 
-    })
+    }
 
-    $('#sub').click(function () { //Minusknappen
+    function sub() { //minusfunktion
 
+        let addNumber = parseFloat($('#text').val())
 
-        let addNumber = parseInt($('#text').val())
-        numbersAdded.push(-addNumber)
+        if (numbersAdded == 0) { //tar bort uträkning för en ny uträkning
+            $('#sumP').html('');
+        }
 
-        if (numbersAdded.length > 1) {
-            calcText = $('#text').val();
-            $('#calc').append(' - ' + calcText);
-            calcText = '';
+        if (!isNaN(addNumber)) { //Checkar så att det är ett nummer innan det läggs till i arrayen
+            numbersAdded.push(-addNumber)
 
-        } else {
+            if (numbersAdded.length > 0) {
+                calcText = $('#text').val();
+                $('#calc').append(' - ' + calcText);
+                $('#text').val('')
 
-            //lägger till i HTML
-            calcText = $('#text').val();
-            $('#calc').append(calcText);
-            calcText = '';
+            } else {
+
+                //lägger till i HTML
+                calcText = $('#text').val();
+                $('#calc').append(calcText);
+                $('#text').val('')
+
+            }
 
         }
 
-    })
+    }
 
-    $('#sum').click(function () { //Summeraknappen
-
+    function sum() { //funktion att summera talet 
         let reduce = (a, b) => a + b;
         let sum = (numbersAdded.reduce(reduce));
         $('#sumP').html(sum)
+        $('#calc').html('');
+        numbersAdded = [];
+
+    }
+
+    $('#add').click(function () { //Plusknappen
+        add()
     })
+
+    $('#sub').click(function () { //Minusknappen
+        sub()
+    })
+
+    $('#sum').click(function () { //Summeraknappen
+        sum();
+    })
+
+
+    //keypress till tangentbordet
+    $(document).keydown(function (e) {
+        switch (e.which) {
+            case 107: //Plustecken
+                add()
+                break;
+            case 187: //Lika-med tecken
+                sum()
+                break;
+            case 109: //Minustecken
+                sub()
+                break;
+            case 13: //Enter triggar lika som lika-med tecken
+                sum()
+        }
+    })
+
+
+    //raderar texten i inputen
+    $(document).keyup(function (e) {
+        switch (e.which) {
+            case 107:
+                $('#text').val('')
+                break;
+            case 187:
+                $('#text').val('')
+                break;
+            case 109:
+                $('#text').val('')
+                break;
+            case 13:
+                $('#text').val('')
+        }
+    })
+
+
 });
+
+
 
 
 //---------TODO--------//
 /*
-tömma all DOM efter uträkningen
+tömma all DOM efter uträkningen fixad
 
-VG - uppgift
+utseende fixad
+
+Annat textsnitt på text i dom? 
+andra finesser?
+
+VG - uppgift fixad
+
+Buggar:
+går ej att få med decimaltal Fixad (parseFloat ist för parseInt)
+Plus och minus blir med i #text-input fixad
+
 */
 
